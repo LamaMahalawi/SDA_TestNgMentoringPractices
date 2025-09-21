8ca3b5b94f0c568d15c03dd9dd4a93ec7d564a1c
Lama-03 <Lama-4321-@hotmail.com>
2025-09-21 22:54:05 +0300

    Update

---
+++ src/test/java/Tasks/TestNG_MorningClass/Day01/C05_SwagLabsTimeOut.java
@@ -0,0 +1,26 @@
+package Tasks.TestNG_MorningClass.Day01;
+
+import Tests.utilities.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+
+public class C05_SwagLabsTimeOut extends TestBase {
+      /*
+    Go to https://www.saucedemo.com/v1/index.html
+    Enter username and password
+    Hit enter
+    Assert 'Products'
+    User must log in in 10 seconds
+     */
+
+    @Test(timeOut = 10000)//ThreadTimeoutException
+    void loginTest() {
+        driver.get("https://www.saucedemo.com/v1/index.html");
+        driver.findElement(By.id("user-name")).sendKeys("performance_glitch_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce", Keys.ENTER);
+        assert driver.findElement(By.xpath("//*[.='Products']")).isDisplayed();
+    }
+
+
+}
---
+++ src/test/java/Tasks/TestNG_MorningClass/Day01/C09_DependsOnMethodTest.java
@@ -0,0 +1,29 @@
+package Tasks.TestNG_MorningClass.Day01;
+
+import Tests.utilities.TestBase;
+import org.testng.annotations.Test;
+
+public class C09_DependsOnMethodTest extends TestBase {
+      /*
+        Create tests that depend on each other
+        First go to Facebook
+        Then go to Google depending on Facebook
+        Then go to Amazon depending on Google
+     */
+
+    @Test
+    void facebookTest() {
+        driver.get("https://facebook.com");
+    }
+
+    @Test(dependsOnMethods = "facebookTest")
+    void googleTest() {
+        driver.get("https://google.com");
+    }
+
+    @Test(dependsOnMethods = {"googleTest","facebookTest"})
+    void amazonTest() {
+        driver.get("https://amazon.com");
+    }
+
+}
